d70e4d0f5d157ba6b9d24256abfe214ef884248b
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-06-07 17:43:00 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-export const Home = ()=> {
- return(
-     <div>HOME</div>
- );
-
-}
-
+export const Home = () => {
+  return <div>HOME page</div>;
+};
 
 export default Home;
