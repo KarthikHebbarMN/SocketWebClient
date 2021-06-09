3ae4717c88378f5bfa7cad5dcdf0a77aa39b5323
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-06-09 20:54:55 +0530

    commit

--- Components/Interactable.js
+++ Components/Interactable.js
@@ -1,13 +1,7 @@
-
 import styles from "./styles.module.css";
 import Tab from "./Tab";
 
-
 const Interactable = () => {
-
-  
-
-
   return (
     <>
       <div className={styles.conatiner}>
@@ -21,11 +15,10 @@ const Interactable = () => {
             </section>
           </div>
           <div className={styles.right_div}>
-            {/* <Img/> */}
-            <div className = {styles.tableDots}></div>
-            <div className = {styles.chairDots}></div>
-            <div className = {styles.floorDots}></div>
-            <div className = {styles.tvDots}></div>
+            <div className={styles.tableDots}></div>
+            <div className={styles.chairDots}></div>
+            <div className={styles.floorDots}></div>
+            <div className={styles.tvDots}></div>
 
             <img
               src='https://images.unsplash.com/photo-1585128792020-803d29415281?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fGhvbWUlMjBpbnRlcmlvcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
