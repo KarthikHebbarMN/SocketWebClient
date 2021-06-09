fbece6807f10ca54341d10e0096d1872d470a0dd
lakshmipriya9392 <lakshpriya9392@gmail.com>
2021-06-09 09:45:13 +0530

    userflow second commit

--- Components/Userflow/progress.js
+++ Components/Userflow/progress.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from '../pages/styles.module.css'
 import { makeStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
@@ -51,7 +50,7 @@ function getStepContent(step) {
 }
 
 
-export default function App() {
+export default function Progress() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Progress from '../Components/Userflow/progress'
 
 export const Home = ()=> {
  return(
-     <div>HOME</div>
- );
+  <Progress />
+  );
 
 }
 
