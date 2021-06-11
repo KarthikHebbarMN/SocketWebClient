0ba5af0088eb36853cb1c02fb464bc3ccd4d7c02
Karthik Hebbar <karthikhebbar600@gmail.com>
2021-06-11 23:24:45 +0530

    WIP fix for aws amplify

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React,{Fragment} from "react";
 
 import Hero from "../components/Hero/Hero";
 import Cards from "../components/cards/Cards";
 import Items from "../components/items/Items";
 import FAQ from "../components/FAQ/FAQ";
 import Footer from "../components/Footer/Footer";
-import Progress from "../components/Userflow/progress";
+import Progress from "../components/UserFlow";
 import Interactable from "../components/Interactable/Interactable";
 
 export const Home = () => {
   return (
-    <>
+   <Fragment>
       <Hero />
       <Cards />
       <Items />
@@ -18,7 +18,7 @@ export const Home = () => {
       <Progress />
       <FAQ />
       <Footer />
-    </>
+      </Fragment>
   );
 };
 
