61618cc154d40fd8de863269ed9aebd060d02745
Jatin204s <Jatin204s@gmail.com>
2021-06-14 16:25:07 +0530

    tenth

--- components/items/AwesomeChair/AwesomeChair.js
+++ components/items/AwesomeChair/AwesomeChair.js
@@ -9,33 +9,24 @@ export const AwesomeChair = () => {
         <div className={styles.itemsdiv}>
           <div className={styles.first}>
             <h2 className={styles.awesome}>Awesome Chair</h2>
-            <h5 className={styles.awesome}>Rs 3,999</h5>
             <button className={styles.awesomebtn}>Buy now</button>
           </div>
           <div className={styles.second}>
             <h2 className={styles.awesome}>Awesome Chair</h2>
-            <h5 className={styles.awesome}>Rs 3,999</h5>
             <button className={styles.awesomebtn}>Buy now</button>
           </div>
         </div>
 
         <div className={styles.third}>
-          <div>
-            <h2 className={styles.awesome1}>Awesome Chair</h2>
-            <h5 className={styles.awesome2}>Rs 3,999</h5>
-            <button className={styles.awesomebtn}>Buy now</button>
-          </div>
-          <div className={styles.thirdchair}>
-            <Image
-              className={styles.thirdchair}
-              src="/images/Rectangle690.png"
-              alt="Picture of the author"
-              width={350}
-              height={500}
-            />
-          </div>
+          <h2 className={styles.awesome1}>Awesome Chair</h2>
+          <Image
+            className={styles.thirdchair}
+            src="/images/Rectangle690.png"
+            alt="Picture of the author"
+            width={350}
+            height={500}
+          />
         </div>
-        <button className={styles.letsgo}>Let's Go</button>
       </div>
     </>
   );
