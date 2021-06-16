4c23df9d508ecf3e40b6e31b99f4c150203483fc
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-06-16 12:19:11 +0530

    commit

---
+++ component/Home/HomePage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+const HomePage = () => {
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.container}>
+          <div class={styles.text_box}>
+            <h1>
+              Refine the tone
+              <br /> of your space
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              cursus efficitur massa, eget porttitor ante malesuada ut. Vivamus
+              interdum rhoncus velit at bibendum. Nunc augue tellus, convallis
+              eget orci at, condimentum maximus ligula. Praesent a massa augue.
+              Suspendisse potenti.
+            </p>
+            Visit Us To know More
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
