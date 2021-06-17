4e082458186f8ff067a52fda12ddd4f1f61098bd
lakshmipriya9392 <lakshpriya9392@gmail.com>
2021-06-17 17:42:00 +0530

    final commit

---
+++ component/FAQ/FAQ.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "../FAQ/FAQ.module.css";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+
+export const FAQ = () => {
+  return (
+    <>
+      <div className={styles.faq}>
+        <h1 className={styles.faqheading}>Frequently Asked Questions</h1>
+
+        <div className={styles.list1}>
+          <ul className={styles.listblock}>
+            <li className={styles.listitems}>
+              Duis aute irure dolor in reprehenderit in voluptate velit?{" "}
+              <a href="" className={styles.arrow}>
+                <ArrowForwardIosIcon />
+              </a>
+            </li>
+            <li className={styles.listitems}>
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+              <a href="" className={styles.arrow}>
+                <ArrowForwardIosIcon />
+              </a>
+            </li>
+            <li className={styles.listitems}>
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+              <a href="" className={styles.arrow}>
+                <ArrowForwardIosIcon />
+              </a>
+            </li>
+            <li className={styles.listitems}>
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+              <a href="" className={styles.arrow}>
+                <ArrowForwardIosIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.list2}></div>
+        <ul className={styles.listblock1}>
+          <li className={styles.listitems}>
+            Duis aute irure dolor in reprehenderit in voluptate velit?
+            <a href="" className={styles.arrow}>
+              <ArrowForwardIosIcon />
+            </a>
+          </li>
+          <li className={styles.listitems}>
+            Duis aute irure dolor in reprehenderit in voluptate velit?
+            <a href="" className={styles.arrow}>
+              <ArrowForwardIosIcon />
+            </a>
+          </li>
+          <li className={styles.listitems}>
+            Duis aute irure dolor in reprehenderit in voluptate velit?
+            <a href="" className={styles.arrow}>
+              <ArrowForwardIosIcon />
+            </a>
+          </li>
+          <li className={styles.listitems}>
+            Duis aute irure dolor in reprehenderit in voluptate velit?
+            <a href="" className={styles.arrow}>
+              <ArrowForwardIosIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default FAQ;
