d5595a7d7de17b15eb2d7fc17a93d101aedabf09
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-06-21 20:42:19 +0530

    commit

--- component/Home/HomePage.js
+++ component/Home/HomePage.js
@@ -1,20 +1,23 @@
-import React,{Fragment} from "react";
-import Meta from '../seo/Meta'
+import React, { Fragment } from "react";
+import Meta from "../seo/Meta";
 import styles from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <Fragment>
       <div className={styles.header}>
         <div className={styles.container}>
-          <div class={styles.text_box}>
-          <img src="https://larcel-image-storage.s3.ap-south-1.amazonaws.com/BlackMarble_Wall_.jpg"/>
+          <div className={styles.text_box}>
+            <img src='https://larcel-image-storage.s3.ap-south-1.amazonaws.com/BlackMarble_Wall_.jpg' />
             <h1>
               Refine the tone
               <br /> of your space
             </h1>
-            <Meta title="Refine the tone" desc=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            <Meta
+              title='Refine the tone'
+              desc=' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
             cursus efficitur massa, eget porttitor ante malesuada ut. Vivamus
-            interdum rhoncus velit at bibendum. Nunc augue tellus"/>
+            interdum rhoncus velit at bibendum. Nunc augue tellus'
+            />
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
               cursus efficitur massa, eget porttitor ante malesuada ut. Vivamus
