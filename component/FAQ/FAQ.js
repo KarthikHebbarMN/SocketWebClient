862df83b5e263e7761d5fd340d81b31f5edc26b5
Jatin204s <Jatin204s@gmail.com>
2021-06-21 21:11:40 +0530

    sixteenth commit

--- component/FAQ/FAQ.js
+++ component/FAQ/FAQ.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styles from "../FAQ/FAQ.module.css";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export const FAQ = () => {
   return (
@@ -8,61 +14,161 @@ export const FAQ = () => {
       <h1 className={styles.faqheading}>Frequently Asked Questions</h1>
       <div className={styles.faq}>
         <div className={styles.list1}>
-          <ul className={styles.listblock}>
-            <li className={styles.listitems}>
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              className={styles.summary}
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
               Duis aute irure dolor in reprehenderit in voluptate velit?
-              <a href="" className={styles.arrow}>
-                <ArrowForwardIosIcon />
-              </a>
-            </li>
-            <li className={styles.listitems}>
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
               Duis aute irure dolor in reprehenderit in voluptate velit?
-              <a href="" className={styles.arrow}>
-                <ArrowForwardIosIcon />
-              </a>
-            </li>
-            <li className={styles.listitems}>
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
               Duis aute irure dolor in reprehenderit in voluptate velit?
-              <a href="" className={styles.arrow}>
-                <ArrowForwardIosIcon />
-              </a>
-            </li>
-            <li className={styles.listitems}>
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
               Duis aute irure dolor in reprehenderit in voluptate velit?
-              <a href="" className={styles.arrow}>
-                <ArrowForwardIosIcon />
-              </a>
-            </li>
-          </ul>
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
         </div>
 
-        <div className={styles.list2}></div>
-        <ul className={styles.listblock1}>
-          <li className={styles.listitems}>
-            Duis aute irure dolor in reprehenderit in voluptate velit?
-            <a href="" className={styles.arrow}>
-              <ArrowForwardIosIcon />
-            </a>
-          </li>
-          <li className={styles.listitems}>
-            Duis aute irure dolor in reprehenderit in voluptate velit?
-            <a href="" className={styles.arrow}>
-              <ArrowForwardIosIcon />
-            </a>
-          </li>
-          <li className={styles.listitems}>
-            Duis aute irure dolor in reprehenderit in voluptate velit?
-            <a href="" className={styles.arrow}>
-              <ArrowForwardIosIcon />
-            </a>
-          </li>
-          <li className={styles.listitems}>
-            Duis aute irure dolor in reprehenderit in voluptate velit?
-            <a href="" className={styles.arrow}>
-              <ArrowForwardIosIcon />
-            </a>
-          </li>
-        </ul>
+        <div className={styles.list2}>
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            className={styles.accord}
+            style={{ backgroundColor: "#333333", color: "white" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}
+            >
+              Duis aute irure dolor in reprehenderit in voluptate velit?
+            </AccordionSummary>
+            <AccordionDetails
+              style={{ backgroundColor: "#FFFAED", color: "black" }}
+            >
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit Duis
+                aute irure dolor in reprehenderit in voluptate velit
+              </p>
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </div>
     </>
   );
