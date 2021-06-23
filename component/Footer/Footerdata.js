e3d37604d55ba6bcb269792babbe6af4221c28b7
Jatin204s <Jatin204s@gmail.com>
2021-06-23 14:56:22 +0530

    Dynamic Footer

---
+++ component/Footer/Footerdata.js
@@ -0,0 +1,27 @@
+const Footerdata = [
+  {
+    title: "Working with wireframes may be a architecture to the visual design",
+    heading: "Store",
+    subheading1: "catalog",
+    subheading2: "Popular",
+    subheading3: "Feature",
+    subheading4: "F.A.Q",
+  },
+  {
+    title: "Working with wireframes may be a architecture to the visual design",
+    heading: "About",
+    subheading1: "catalog",
+    subheading2: "Popular",
+    subheading3: "Features",
+  },
+
+  {
+    title: "Working with wireframes may be a architecture to the visual design",
+    heading: "Catalog",
+    subheading1: "catalog",
+    subheading2: "Popular",
+    subheading3: "Features",
+  },
+];
+
+export default Footerdata;
