8d37f5bda5b96c8f2b7957db991ba296ce2257a6
Jatin204s <Jatin204s@gmail.com>
2021-06-24 11:48:54 +0530

    19th commit

--- component/items/AwesomeChair/AwesomeChair.js
+++ component/items/AwesomeChair/AwesomeChair.js
@@ -22,35 +22,3 @@ function AwesomeChair() {
 }
 
 export default AwesomeChair;
-
-// export const AwesomeChair = () => {
-// const cardinfo = [
-//   {
-//     title: "Awesome Chair",
-//     price: "Rs 3,999",
-//   },
-//   {
-//     title: "Awesome Chair",
-//     price: "Rs 3,999",
-//   },
-//   {
-//     title: "Awesome Chair",
-//     price: "Rs 3,999",
-//   },
-// ];
-
-//   const renderCard = (card, index) => {
-//     return (
-//       <div className={styles.itemsdiv} key={index}>
-//         <div className={styles.first}>
-//           <h2 className={styles.awesome}>{card.title}</h2>
-//           <h4 className={styles.awesome1}>{card.price}</h4>
-//           <button className={styles.awesomebtn}>Buy now</button>
-//         </div>
-//       </div>
-//     );
-//   };
-//   return <>{cardinfo.map(renderCard)}</>;
-// };
-
-// export default AwesomeChair;
