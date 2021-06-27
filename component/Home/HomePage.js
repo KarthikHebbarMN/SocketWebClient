ac3cb974b07e45d8470f2aa3216b79b85ef7bff2
Karthik Hebbar <karthikhebbar600@gmail.com>
2021-06-27 19:56:12 +0530

    Test SSL Tickets

--- component/Home/HomePage.js
+++ component/Home/HomePage.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const HomePage = () => {
 useEffect(async()=>{
   console.log("Fetch form End pont");
-  let resp = await axios.get("https://35.154.229.151:443/res/api/v1/landing/landing");
+  let resp = await axios.get("https://35.154.229.151:443/rest/api/v1/landing/landing");
   console.log(resp);
 },[]);
   return (
